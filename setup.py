b8e56a00e6b21e000ed9236af04b033b532fd976
Alex Ntavelos <a.ntavelos@gmail.com>
2021-11-05 14:53:53 +0100

    bump version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ __author__ = 'EUROCONTROL (SWIM)'
 
 setup(
     name='swim-qpid-proton',
-    version='0.0.2',
+    version='0.0.3',
     description='qpid-proton wrapper',
     author='EUROCONTROL (SWIM)',
     author_email='',
