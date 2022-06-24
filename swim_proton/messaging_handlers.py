f42fe2b3e33cd70fc972841b2e18413efed40bb2
kjenod <107796030+kjenod@users.noreply.github.com>
2022-06-24 10:05:21 +0200

    Add files via upload

--- swim_proton/messaging_handlers.py
+++ swim_proton/messaging_handlers.py
@@ -212,7 +212,7 @@ class Producer(PubSubMessagingHandler):
     def _create_sender_link(self, endpoint: str) -> Optional[proton.Sender]:
         try:
             self.container.create_sender(self.connection, endpoint)
-            _logger.debug(f"Created sender on endpoint {self.endpoint}")
+          #  _logger.debug(f"Created sender on endpoint {self.endpoint}")
         except Exception as e:
             _logger.error(f'Failed to create sender on endpoint {self.endpoint}: {str(e)}')
             sender = None
